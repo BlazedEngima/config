d9651a5c85f02406a27b1d64f5e3e75b6598b9c1
Samuel Theofie <samueltheofie@gmail.com>
2026-05-11 19:30:22 +0100

    Removed useless file

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,10 @@
 return {
   "folke/snacks.nvim",
+  opts = {
+    scroll = {
+      enabled = false, -- Disable scrolling animations
+    },
+  },
   keys = {
     { "<leader>/", false },
     { "<leader>fT", false },
