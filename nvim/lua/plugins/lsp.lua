7925728680e72b2ff1050692d279491919fe00fd
Samuel Theofie <samueltheofie@gmail.com>
2026-05-11 18:51:07 +0100

    Added clangd LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,33 @@
 return {
   "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
   opts = {
+    ---@type lspconfig.options
     servers = {
+      pyright = {},
+      clangd = {
+        settings = {
+          ["clangd"] = {
+            inlayHints = {
+              bindingModeHints = { enable = true },
+              chainingHints = { enable = true },
+              closingBraceHints = { enable = true },
+              closureReturnTypeHints = { enable = "always" },
+              lifetimeElisionHints = { enable = "always" },
+              parameterHints = { enable = true },
+              typeHints = { enable = true },
+            },
+          },
+        },
+
+        inlay_hints = {
+          enabled = true,
+        },
+
+        code_lens = {
+          enabled = true,
+        },
+      },
       rust_analyzer = {
         settings = {
           ["rust-analyzer"] = {
