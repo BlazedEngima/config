fc581141284dffd6196f652cdddfa8e541aadf89
Samuel Theofie <samueltheofie@gmail.com>
2026-05-11 19:25:10 +0100

    Added Neo Tree mappings

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,13 +5,30 @@ return {
       -- disable LazyVim default
       { "<leader>e", false },
 
-      -- add your new binding
+      -- New neo tree mapping
       {
         "<C-n>",
         function()
           require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
         end,
         desc = "NeoTree (cwd)",
+      },
+    },
+    opts = {
+      window = {
+        mappings = {
+          ["<C-v>"] = "open_vsplit",
+          ["<C-h>"] = "open_split",
+          ["e"] = function()
+            vim.cmd("Neotree focus filesystem left", true)
+          end,
+          ["b"] = function()
+            vim.cmd("Neotree focus buffers left", true)
+          end,
+          ["g"] = function()
+            vim.cmd("Neotree focus git_status left", true)
+          end,
+        },
       },
     },
   },
