aaac9253885ca66fe0c1af79751d08d0324fbce8
Samuel Theofie <samueltheofie@gmail.com>
2026-05-11 18:51:39 +0100

    TODO: Fix auto tab closing autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,25 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local id = vim.api.nvim_create_augroup("startup", {
+  clear = false,
+})
+
+local persistbuffer = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  vim.fn.setbufvar(bufnr, "bufpersist", 1)
+end
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufEnter", "tabnew" }, {
+  group = id,
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+      buffer = 0,
+      once = true,
+      callback = function()
+        persistbuffer()
+      end,
+    })
+  end,
+})
